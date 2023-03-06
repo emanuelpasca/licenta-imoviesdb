9b5ccbb941438933d33cb750cd5b370d6aff52c5
emanuelpasca <pasca.emanuel@yahoo.com>
2023-03-06 23:47:48 +0200

    fixed favorites context bug

--- src/contexts/UserFavoritesContext.jsx
+++ src/contexts/UserFavoritesContext.jsx
@@ -1,25 +1,52 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { whereQuery } from "../configs/firebase/actions";
 import { useUserAuth } from "./AuthContext";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../configs/firebase";
 
 const userFavoritesContext = createContext();
 
 export const UserFavoritesContextProvider = ({ children }) => {
-  const [userFavorites, setUserFavorites] = useState(null);
   const { user } = useUserAuth();
 
+  const [userFavorites, setUserFavorites] = useState([]);
+
   useEffect(() => {
-    if (user) {
-      whereQuery("favorites", "userId", user.uid).then((results) => {
-        setUserFavorites(results);
-      });
-    } else {
-      setUserFavorites(null);
-    }
-  }, [user]);
+    // Listen for changes in authentication state
+    // const unsubscribe = onAuthStateChanged(auth, (user) => {
+    //   if (user) {
+    //     whereQuery("favorites", "userId", user.uid).then((results) => {
+    //       setUserFavorites(results);
+    //       console.log("USER FAVORITES RESULTS:");
+    //       console.log(results);
+    //     });
+    //   } else {
+    //     setUserFavorites(null);
+    //   }
+    // });
+
+    // // Unsubscribe from the listener when the component unmounts
+    // return () => unsubscribe();
+
+    const fetchFavorites = async () => {
+      if (user) {
+        const favorites = await whereQuery("favorites", "userId", user.uid);
+        setUserFavorites(favorites);
+      } else {
+        setUserFavorites(null);
+      }
+    };
+
+    fetchFavorites();
+  }, []);
 
   return (
-    <userFavoritesContext.Provider value={{ userFavorites, setUserFavorites }}>
+    <userFavoritesContext.Provider
+      value={{
+        userFavorites,
+        setUserFavorites,
+      }}
+    >
       {children}
     </userFavoritesContext.Provider>
   );
